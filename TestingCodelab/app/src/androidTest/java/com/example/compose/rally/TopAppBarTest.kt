328b7efdaca587b217dfafa2e30395cd26190c48
Máté Iszlai <iszimate95@gmail.com>
2023-02-16 12:00:49 +0100

    Create TopAppBarTest.kt

---
+++ TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -0,0 +1,24 @@
+package com.example.compose.rally
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import com.example.compose.rally.ui.components.RallyTopAppBar
+import org.junit.Rule
+import org.junit.Test
+
+class TopAppBarTest {
+    @get: Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun rallyTopBarTest(){
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = {},
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+        Thread.sleep(5000)
+    }
+}
