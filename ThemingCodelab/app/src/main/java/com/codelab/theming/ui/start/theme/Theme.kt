6b6d1c90c3b45a332a982749f0e4e27f323c1e68
Máté Iszlai <iszimate95@gmail.com>
2023-02-16 12:47:11 +0100

    Shapes added

--- ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
+++ ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
@@ -22,6 +22,7 @@ fun JetNewsTheme(
     MaterialTheme(
         content = content,
         colors = LightColors,
-        typography = JetnewsTypography
+        typography = JetnewsTypography,
+        shapes = JetnewsShapes
     )
 }
