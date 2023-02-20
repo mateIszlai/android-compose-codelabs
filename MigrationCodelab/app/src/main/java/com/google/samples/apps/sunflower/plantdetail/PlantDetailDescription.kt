bc6ffb97b007f9ec6b429b1cdb3a3d8d293970d6
Máté Iszlai <iszimate95@gmail.com>
2023-02-20 12:00:42 +0100

    Apply theming

--- MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,6 +16,7 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import android.content.res.Configuration
 import android.text.method.LinkMovementMethod
 import android.widget.TextView
 import androidx.compose.foundation.layout.Column
@@ -39,6 +40,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.text.HtmlCompat
+import com.google.accompanist.themeadapter.material.MdcTheme
 import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
 import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
@@ -90,7 +92,7 @@ private fun PlantWatering(wateringInterval: Int) {
 )
 @Composable
 fun PlantWateringPreview() {
-    MaterialTheme {
+    MdcTheme {
         PlantWatering(wateringInterval = 7)
     }
 }
@@ -118,7 +120,7 @@ private fun PlantDescription(description: String) {
 @Preview
 @Composable
 fun PlantDescriptionPreview() {
-    MaterialTheme {
+    MdcTheme {
         Surface {
             PlantDescription(description = "HTML<br><br>description")
         }
@@ -143,7 +145,9 @@ fun PlantDetailContent(plant: Plant) {
     }
 }
 
-@Preview
+@Preview(
+    showBackground = true
+)
 @Composable
 fun PlantDetailDescription() {
     val plant = Plant(
@@ -154,7 +158,24 @@ fun PlantDetailDescription() {
         30,
         ""
     )
-    Surface {
+    MdcTheme {
         PlantDetailContent(plant = plant)
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun PlantDetailContentDarkPreview() {
+    val plant = Plant(
+        "id",
+        "Apple",
+        "HTML<br><br>description",
+        3,
+        30,
+        ""
+    )
+
+    MdcTheme {
+        PlantDetailContent(plant)
     }
 }
