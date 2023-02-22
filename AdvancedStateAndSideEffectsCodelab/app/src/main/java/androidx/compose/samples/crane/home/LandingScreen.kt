1b70b60c42e3b946cf813437045b4cbe5e522710
Máté Iszlai <iszimate95@gmail.com>
2023-02-22 16:01:27 +0100

    Implement state holder

--- AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
+++ AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
@@ -42,7 +42,6 @@ fun LandingScreen(modifier: Modifier = Modifier, onTimeout: () -> Unit) {
             currentOnTimeout()
         }
 
-
         Image(painterResource(id = R.drawable.ic_crane_drawer), contentDescription = null)
     }
 }
